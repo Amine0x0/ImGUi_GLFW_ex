c6f71c921105a005542c02137db54772162abcef
Amine Abbadi <amabbadi@e2r4p14.1337.ma>
2026-03-28 17:38:10 +0100

    first experiment, so fucking confusing to say the least

---
+++ Makefile
@@ -0,0 +1,34 @@
+CC = c++ -std=c++11
+FLAGS = -Wall -Wextra -g3 -fsanitize=address
+SRC_FOLDER = ./src
+INCL_FOLDER = ./incl
+IMGUI_FOLDER = ./imgui
+BACKEND_FOLDER = ./imgui/backends
+
+SRCS = $(wildcard $(SRC_FOLDER)/*.cpp) \
+       $(wildcard $(IMGUI_FOLDER)/*.cpp) \
+       $(BACKEND_FOLDER)/imgui_impl_glfw.cpp \
+       $(BACKEND_FOLDER)/imgui_impl_opengl3.cpp
+
+OBJS = $(SRCS:.cpp=.o)
+EXEC = app
+
+INCLUDES = -I$(INCL_FOLDER) -I$(IMGUI_FOLDER) -I$(BACKEND_FOLDER)
+
+LIBS = -lglfw -lGL -ldl -lpthread
+
+all: $(EXEC)
+
+$(EXEC): $(OBJS)
+	$(CC) $(FLAGS) $(OBJS) -o $@ $(LIBS)
+
+%.o: %.cpp
+	$(CC) $(FLAGS) $(INCLUDES) -c $< -o $@
+
+re: fclean all
+
+clean:
+	rm -f $(OBJS) $(EXEC)
+
+fclean: clean
+	rm -rf $(EXEC)
---
+++ src/main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include <GLFW/glfw3.h>
+
+namespace App{
+    typedef struct{
+        GLFWwindow* window;
+        ImVec4 clear_color;
+        bool show_window_demo;
+    }   Main_Window;
+
+    class init_except : public std::exception{
+        virtual const char *what() const throw(){
+            return "INIT_ERROR";
+        }
+    };
+
+    class aborted_window : public std::exception{
+        virtual const char *what() const throw(){
+            return "window aborted abnormaly, maybe ESC";
+        }
+    };
+
+    Main_Window *init_backend(){
+        Main_Window* main = new Main_Window;
+
+        GLFWwindow* window = glfwCreateWindow(1280, 800, "DEBUGGING WINDOW", nullptr, nullptr);
+        if (!window)
+            throw init_except();
+        main->window = window;
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+        ImGui::StyleColorsDark();
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 130");
+
+        ImVec4 clear_color = ImVec4(0.2f, 0.2f, 0.2f, 0.2f);
+        main->clear_color = clear_color;
+
+        bool show_demo_window = false;
+        main->show_window_demo = show_demo_window;
+        return main;
+    }
+
+    [[noreturn]] inline void emergency_leave(){
+        exit(0);
+    }
+
+    void WindowLoop(Main_Window* window_main){
+        while (!glfwWindowShouldClose(window_main->window)) {
+            glfwPollEvents();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            ImGui::ShowDemoWindow(&window_main->show_window_demo);
+
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window_main->window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(window_main->clear_color.x, window_main->clear_color.y, window_main->clear_color.z, window_main->clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            glfwSwapBuffers(window_main->window);
+        }
+        throw(aborted_window());
+    }
+
+    void Window_running(Main_Window* window_main){
+        try{
+            window_main = init_backend();
+        }
+        catch (std::exception& e){
+            std::cerr << e.what() << std::endl;
+            emergency_leave();
+        }
+        try{
+            WindowLoop(window_main);
+        }
+        catch(std::exception& e){
+            std::cerr << e.what() << std::endl;
+            emergency_leave();
+        }
+    }
+
+    void cleanup(Main_Window* window_main){
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        glfwDestroyWindow(window_main->window);
+        glfwTerminate();
+    }
+}
+
+int main() {
+    if (!glfwInit())
+        return 1;
+
+    App::Main_Window* window_main{};
+
+    App::Window_running(window_main);
+    App::cleanup(window_main);
+    return 0;
+}
